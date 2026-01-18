c6c6bf619412f537e574697f869a575b0605e268
Vitya <@aaa>
2026-01-18 15:21:18 +0300

    это для обоих файлов?

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
 print("hello world")
 print("gello tuf")
+print("farfwert")
+a = int(input())
+if a == 67:
+    print("67676767676767676767676767676767676767676767676677")
+else:
+    print("no 67((((")
--- second.py
+++ second.py
@@ -1,3 +1,4 @@
 print("AAAAAAAAAAAAAA")
 print("ну енто тест")
 print(*[i for i in range(10) if i % 2 == 0])
+print("deletethisplz")
