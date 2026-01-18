5c6a173fb8e30c9fa5558eafc30c0cfe6e76ef16
VityaBlebik <165920022+VityaBlebik@users.noreply.github.com>
2026-01-18 16:47:12 +0300

    Update first.py
    bvv

--- first.py
+++ first.py
@@ -2,7 +2,3 @@ print("hello world")
 print("gello tuf")
 print("farfwert")
 a = int(input())
-if a == 67:
-    print("67676767676767676767676767676767676767676767676677")
-else:
-    print("no 67((((")
